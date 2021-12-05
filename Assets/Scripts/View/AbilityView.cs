7dc576a041a0e6979ccf3f09f519185e96f8156f
RomanKhodakov <pomah_654@mail.ru>
2021-12-05 20:19:32 +0300

    Рефактор кнопки старта и добавил самоуничтожение бомб

--- Assets/Scripts/View/AbilityView.cs
+++ Assets/Scripts/View/AbilityView.cs
@@ -1,8 +1,21 @@
+using System.Collections;
 using UnityEngine;
 
 public sealed class AbilityView : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float _selfDestructionTime = 2f;
 
     public Rigidbody2D ObjectRigidbody2D => _rigidbody2D;
+
+    private void Awake()
+    {
+        StartCoroutine(SelfDestroy());
+    }
+
+    private IEnumerator SelfDestroy()
+    {
+        yield return new WaitForSeconds(_selfDestructionTime);
+        Destroy(gameObject);
+    }
 }
