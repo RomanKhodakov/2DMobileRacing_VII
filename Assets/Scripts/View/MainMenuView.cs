09cfbd2331413ea75135b56e3103132259e84a87
RomanKhodakov <pomah_654@mail.ru>
2021-12-14 23:19:02 +0300

    Добавил пуш уведомление и локализацию кнопок

--- Assets/Scripts/View/MainMenuView.cs
+++ Assets/Scripts/View/MainMenuView.cs
@@ -1,20 +1,34 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class MainMenuView : MonoBehaviour
 {
     [SerializeField] 
-    private Button _buttonStart;
+    private Button _buttonStart; 
+    
+    [SerializeField] 
+    private Button _buttonNotification;
+    
+    [SerializeField] 
+    private Button _buttonRuLanguage;
+    
+    [SerializeField] 
+    private Button _buttonEnLanguage;
         
-    public void SubscribeAction(UnityAction startGame)
+    public void SubscribeAction(UnityAction startGame, UnityAction createNotification, Action<int> changeLanguage)
     {
         _buttonStart.onClick.AddListener(startGame);
+        _buttonNotification.onClick.AddListener(createNotification);
+        _buttonRuLanguage.onClick.AddListener(() => changeLanguage(0));
+        _buttonEnLanguage.onClick.AddListener(() => changeLanguage(1));
     }
 
     protected void OnDestroy()
     {
         _buttonStart.onClick.RemoveAllListeners();
+        _buttonNotification.onClick.RemoveAllListeners();
     }
 }
 
